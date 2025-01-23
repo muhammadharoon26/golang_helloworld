86532a9668ac1857575a7c035094378a8667bbc1
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-23 16:55:25 +0500

    `Added new variables and constants to main.go`

--- main.go
+++ main.go
@@ -13,9 +13,26 @@ func main() {
 	var version = "2.0"
 	var money int = 200000
 	var currency = 200000
+	var dimension float64 = 101.101
+	var decided bool = false
+	decided = true
 
 	fmt.Println("My name is", name, "!")
 	fmt.Println("Its", version)
 	fmt.Println("money:", money)
 	fmt.Println("currency:", currency)
+	fmt.Println(dimension)
+	fmt.Println(decided)
+
+	const pi = 67.12
+	fmt.Println("pi value:", pi)
+
+	person := 123
+	fmt.Println(person)
+
+	var Public = "data is important"
+	var private = "data is private"
+
+	fmt.Println(Public)
+	fmt.Println(private)
 }
---
+++ print/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "Alice"
+	height := 5.8234567
+
+	fmt.Println("age:", age, "name:", name, "height:", height)
+	fmt.Println("Hello World !")
+}
