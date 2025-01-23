85ccdccb41ce52c6571b457cc79e5ff57b88e48a
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-23 16:29:31 +0500

    `Added new variables and print statements to main function`

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	fmt.Println("Hello World!")
-
 	myutil.PrintMessage("Hello World")
+
+	var name string = "My name is Haroon!"
+	var version = "2.0"
+	var money int = 200000
+	var currency = 200000
+
+	fmt.Println(name)
+	fmt.Println(version)
+	fmt.Println(money)
+	fmt.Println("currency: ", currency)
 }
