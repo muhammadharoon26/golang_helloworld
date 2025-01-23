f62a476fa9777e46de4fcdc074c2d264cbe76fda
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-23 16:32:25 +0500

    `Update main.go: replace string variable name, reorder and modify print statements`

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ func main() {
 	fmt.Println("Hello World!")
 	myutil.PrintMessage("Hello World")
 
-	var name string = "My name is Haroon!"
+	var name string = "Haroon"
 	var version = "2.0"
 	var money int = 200000
 	var currency = 200000
 
-	fmt.Println(name)
-	fmt.Println(version)
-	fmt.Println(money)
-	fmt.Println("currency: ", currency)
+	fmt.Println("My name is", name, "!")
+	fmt.Println("Its", version)
+	fmt.Println("money:", money)
+	fmt.Println("currency:", currency)
 }
