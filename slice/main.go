29991d653c70ef9684223cbdb0d47dfc35b8cf4a
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-25 00:16:25 +0500

    `Added import statement and code to demonstrate Go slices`

--- slice/main.go
+++ slice/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{1, 2, 3, 4, 5}
+	fmt.Println("Numbers array: ", numbers)
+	fmt.Println("Length:", len(numbers))
+	numbers = append(numbers, 6, 7, 8, 9, 10)
+	fmt.Println("Numbers array: ", numbers)
+	fmt.Println("Length:", len(numbers))
+
+	name := []string{}
+	fmt.Println("Name array: ", name)
 }
