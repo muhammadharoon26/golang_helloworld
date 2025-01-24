151b0fdcc52486508bd4d9d7c7fe88ed6d4dcc44
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-24 18:59:34 +0500

    `Added array examples and usage in main.go`

--- array/main.go
+++ array/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("we are learning array in golang.")
+
+	var name [5]string
+	name[0] = "prince"
+	name[2] = "haroon"
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println("Names of person is:", name)
+	fmt.Println("Numbers are:", numbers)
+	fmt.Println("Length of Numbers array is:", len(numbers))
+	fmt.Println("Length of Name array is:", len(name))
+	fmt.Println("value of name at 2 index is:", name[2])
+	fmt.Println("----------------------------------------")
+	fmt.Println("----------------------------------------")
+
+	var price [5]int
+	fmt.Println("price is:", price)
 }
