17fd2fe2aa9e2b83042cb18816af18a8b9a9045d
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-24 18:32:54 +0500

    `Added divide function with error handling and called it in main function`

--- error_handling/main.go
+++ error_handling/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("denominator must not be zero")
+	}
+	return a / b, nil
+}
+
 func main() {
 	fmt.Println("started error handling in the functions.")
+	ans, err := divide(10, 2)
+	if err != nil {
+		fmt.Println("Error Handling")
+	}
+	fmt.Println("The answer of division is:", ans)
 }
