322f36b2e95673227c97f54765e45cb9812b6e9a
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-25 20:39:32 +0500

    `Added fmt import and switch statements for day and month`

--- switch/main.go
+++ switch/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	day := 2
+	switch day {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	default:
+		fmt.Println("Unknown day")
+	}
+
+	month := "August"
+	switch month {
+	case "January", "February", "March":
+		fmt.Println("Winter")
+	case "April", "May", "June":
+		fmt.Println("Spring")
+	default:
+		fmt.Println("Other season")
+	}
 }
