ddf4883232bfdaf147824efa27f0e979bc8833da
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-25 20:51:38 +0500

    `Added import statement and two for loops to main.go`

--- for-loop/main.go
+++ for-loop/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for i := 0; i <= 10; i++ {
+		fmt.Println("Number is:", i)
+	}
+
+	counter := 0
+	for {
+		fmt.Println("Infinite Loop")
+		counter++
+		if counter == 3 {
+			break
+		}
+	}
 }
