380d20646e8fd4076cec09ba9d0379844adb7268
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-27 15:35:45 +0500

    `Added strings package import and string manipulation examples`

--- strings/main.go
+++ strings/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Lets's learn strings in golang")
+
+	data := "apple, orange, banana"
+	fmt.Println(data)
+	parts := strings.Split(data, ",")
+	fmt.Println(parts)
+
+	str := "one two three four five two"
+	count := strings.Count(str, "two")
+	fmt.Println(count)
+
+	str = "            Hello, World!             "
+	fmt.Println(str)
+	fmt.Println(strings.TrimSpace(str))
 }
