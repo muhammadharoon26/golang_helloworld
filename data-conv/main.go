78de40108fbedd4c852ac9bbb10ab80113e6aab0
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-27 15:24:17 +0500

    `Added print statements to demonstrate data conversion in main function`

--- data-conv/main.go
+++ data-conv/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Let's learn data conversion in golang")
+
+	num := 42
+	fmt.Println("The value of num is:", num)
+	fmt.Printf("Type of variable num is %T\n", num)
+
+	var data float64 = float64(num)
+	data = data + 1.23
+	fmt.Println("The value of data is:", data)
+	fmt.Printf("Type of variable num is %T\n", data)
 }
