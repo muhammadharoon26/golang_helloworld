257d692a02d4c1a144e51ef0d107d1baf1f98ae2
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-27 15:11:23 +0500

    `Added modifyValueByReference function and modified main function to demonstrate pointer usage.`

--- pointer/main.go
+++ pointer/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func modifyValueByReference(num *int) {
+	*num = *num * 2
+}
+
+func main() {
+	num := 2
+	ptr := &num
+
+	fmt.Println("Num has value:", num)
+	fmt.Println("pointer contains:", ptr)
+	fmt.Println("value of pointer:", *ptr)
+
+	value := 10
+	modifyValueByReference(&value)
+	fmt.Println("Value contains:", value)
 }
