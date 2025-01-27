08c3a271b4008b446ed40cac069f33ac69a07f66
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-27 14:55:27 +0500

    `Added import statement and Person struct definition to main.go`

--- struct/main.go
+++ struct/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	var haroon Person
+	fmt.Println("Person:", haroon)
+	haroon.FirstName = "m"
+	haroon.LastName = "haroon"
+	haroon.Age = 20
+	fmt.Println("Person:", haroon)
 }
