1010cacbfb35862b0511733ea0b2ae07d99bbc1e
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-28 11:24:21 +0500

    `Added time and goroutine imports, created sayHello and sayHi functions, and added goroutine execution in main function.`

--- goroutine/main.go
+++ goroutine/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello() {
+	fmt.Println("Hello, World!")
+}
+
+func sayHi() {
+	fmt.Println("Hi, World!")
+}
 
 func main() {
 	fmt.Println("Let's learn goroutine in golang.")
+
+	go sayHello()
+	sayHi()
+
+	time.Sleep(1000 * time.Millisecond)
 }
