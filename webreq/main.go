68c87ecaec30ec4563bac3112b323fc68e60cba2
Muhammad Haroon <mharoon1326@gmail.com>
2025-01-28 10:31:03 +0500

    `Added imports for net/http and ioutil, made HTTP GET request to jsonplaceholder API, and read response body.`

--- webreq/main.go
+++ webreq/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Let's learn web requests in golang.")
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer res.Body.Close()
+	fmt.Println("Response:", res)
+	fmt.Printf("Type of response:%T\n", res)
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Data:", data)
 }
